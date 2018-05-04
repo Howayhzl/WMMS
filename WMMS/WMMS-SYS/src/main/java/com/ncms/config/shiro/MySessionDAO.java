f17675563e2dbc287982052670302661fb9996fb
songjianlong-Asix <827856405@qq.com>
2018-05-04 15:35:16 +0800

    clean remark print

--- WMMS/WMMS-SYS/src/main/java/com/ncms/config/shiro/MySessionDAO.java
+++ WMMS/WMMS-SYS/src/main/java/com/ncms/config/shiro/MySessionDAO.java
@@ -13,22 +13,18 @@ public class MySessionDAO extends AbstractSessionDAO {
 	private Map<Serializable, Session> map = new HashMap<Serializable, Session>();
 
 	public void update(Session session) throws UnknownSessionException {
-		System.out.println("now update session");
 		map.put(session.getId(), session);
 	}
 
 	public void delete(Session session) {
-		System.out.println("now delete session");
 		map.remove(session.getId());
 	}
 
 	public Collection<Session> getActiveSessions() {
-		System.out.println("now getActiveSessions session");
 		return map.values();
 	}
 
 	protected Serializable doCreate(Session session) {
-		System.out.println("now doCreate session");
 		Serializable sessionId = generateSessionId(session);
 		assignSessionId(session, sessionId);
 		map.put(sessionId, session);
@@ -37,7 +33,6 @@ public class MySessionDAO extends AbstractSessionDAO {
 	}
 
 	protected Session doReadSession(Serializable sessionId) {
-		System.out.println("now doReadSession session");
 		return map.get(sessionId);
 	}
 }
