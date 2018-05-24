70b170913fccdaf31a906d0870869c9fad1981cc
wesley <xwh88222@gmail.com>
2018-05-25 00:43:39 +0800

    add census and collocation mapper , model

---
+++ WMMS/WMMS-SYS/src/main/java/com/ncms/mapper/meter/census/PrdCensusMapper.java
@@ -0,0 +1,15 @@
+package com.ncms.mapper.meter.census;
+
+import	com.ncms.model.meter.census.PrdCensus;
+
+import java.util.List;
+
+import com.ncms.config.mybatis.MyMapper;
+
+/**
+ * @date 2018-05-23 15:31:02
+ */
+public interface PrdCensusMapper extends MyMapper<PrdCensus> {
+
+
+}
---
+++ WMMS/WMMS-SYS/src/main/java/com/ncms/mapper/meter/census/PrdCollocationMapper.java
@@ -0,0 +1,18 @@
+package com.ncms.mapper.meter.census;
+
+import java.util.List;
+
+import com.ncms.config.mybatis.MyMapper;
+import com.ncms.model.meter.census.PrdCollocation;
+
+
+/** 
+ * @ClassName: PrdCollocationMapper 
+ * @Description: 配表单查询 
+ * @author Wesley.Xia 
+ * @date 2018年5月25日 上午12:37:56 
+ */
+public interface PrdCollocationMapper extends MyMapper<PrdCollocation> {
+
+
+}
